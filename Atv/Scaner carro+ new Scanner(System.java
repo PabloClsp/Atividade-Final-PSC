d3d917d0a799c34e9dfb1b624462ec9db16633da
PaBloC <123667971+PabloClsp@users.noreply.github.com>
2024-03-23 14:50:37 -0300

    Create Scaner carro+ new Scanner(System.java

---
+++ Atv/Scaner carro+ new Scanner(System.java
@@ -0,0 +1,13 @@
+Scaner carro+ new Scanner(System.in); 
+int limite;
+double Res = 0, N =1 M = 1;
+System.out.println("digite um número para limitar o cáuculo");
+limite = carro.next
+
+for(int index = 1; index+)
+System.out.print(N+"/"+M+ "+"){
+ Res += N/M;
+N = N + 1;
+M = M + 2;
+}
+System.out.println(" = " + Res);
